8bad4c6bcf4aa765e568947de8429fb92319595a
Guo Song <song.guo@weblab.t.u-tokyo.ac.jp>
2025-04-16 01:53:58 +0800

    add gsm8k to attack 1.5b

--- scripts/adversarial_test_qwen25_math_1_5b.sh
+++ scripts/adversarial_test_qwen25_math_1_5b.sh
@@ -1,5 +1,4 @@
-# python3 math_pipeline/generative_adversarial.py --data_names math,gsm8k --data_dir outputs/initial_inference \
-python3 math_pipeline/generative_adversarial.py --data_names math --data_dir outputs/initial_inference \
+python3 math_pipeline/generative_adversarial.py --data_names math,gsm8k --data_dir outputs/initial_inference \
     --attacker_model_name_or_path DeepSeekR1 \
     --original_model_name_or_path "Qwen/Qwen2.5-Math-1.5B-Instruct" \
     --output_dir outputs/adversarial_attack --prompt_type qwen25-math-cot --split train \
